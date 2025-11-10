52bd3680ecf854410e09e7ea77b13a1c43303a23
Yanuar Setyoningsih <yanuar.setyoningsih@gmail.com>
2025-11-10 17:05:14 +0700

    Update Contoh.cs

--- Assets/Script/Contoh.cs
+++ Assets/Script/Contoh.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class Contoh : MonoBehaviour
@@ -5,9 +6,21 @@ public class Contoh : MonoBehaviour
     Vector2 position1 = new Vector2(1, 1);
     int number1 = 5;
     int number2 = 7;
-    
+    int usia = 17;
+    int money = 50000;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
+    {
+        Debug.Log(Perkalian(54, 6, 34));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Swap()
     {
         Debug.Log(position1.x + " , " + position1.y);
         Debug.Log("Awal");
@@ -19,13 +32,43 @@ public class Contoh : MonoBehaviour
         number2 = temp;
         Debug.Log("Number 1 : " + number1);
         Debug.Log("Number 2 : " + number2);
-
-        Debug.Log(((1287 * 14 + 589) > (63 + 55 * 5)) || (21 < 10));
     }
 
-    // Update is called once per frame
-    void Update()
+    void CinemaEntry()
     {
-       
+        Debug.Log("Usia : " + usia);
+        Debug.Log("Uang : " + money);
+        if (usia >= 18 || money >= 100000)
+        {
+            Debug.Log("Entry : ALLOWED ");
+        }
+        else
+        {
+            Debug.Log("Entry : PROHIBITED ");
+        }
+    }
+
+    void Weapon(int id)
+    {
+        switch (id)
+        {
+            case 1:
+                Debug.Log("Basic Sword");
+                break;
+            case 2:
+                Debug.Log("Elektric Bow");
+                break;
+            case 3:
+                Debug.Log("Fire Spell");
+                break;
+            default:
+                Debug.Log("Undefined");
+                break;
+        }
+    }
+    
+    int Perkalian(int a, int b, int c)
+    {
+        return a * b * c;
     }
 }
