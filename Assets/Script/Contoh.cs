eede4b5bf47d915abd139f73333bdca2b46326d8
Yanuar Setyoningsih <yanuar.setyoningsih@gmail.com>
2025-11-10 15:39:19 +0700

    commit

--- Assets/Script/Contoh.cs
+++ Assets/Script/Contoh.cs
@@ -2,15 +2,28 @@ using UnityEngine;
 
 public class Contoh : MonoBehaviour
 {
+    Vector2 position1 = new Vector2(1, 1);
+    int number1 = 5;
+    int number2 = 7;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log(position1.x + " , " + position1.y);
+        Debug.Log("Awal");
+        Debug.Log("Number 1 : " + number1);
+        Debug.Log("Number 2 : " + number2);
+        Debug.Log("SWAP");
+        int temp = number1;
+        number1 = number2;
+        number2 = temp;
+        Debug.Log("Number 1 : " + number1);
+        Debug.Log("Number 2 : " + number2);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 }
